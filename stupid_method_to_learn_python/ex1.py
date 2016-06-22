47ebf73a5fccc237ec243ff8722ac6566628318a
谭元元 <tyy_02@163.com>
2016-06-22 18:00:06 +0800

    Create ex1.py

---
+++ stupid_method_to_learn_python/ex1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+__author__ = 'yytan'
+
+print "Hello World!"
+print "Hello again"
+print "I like typing this."
+print "This is fun"
+print 'Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
